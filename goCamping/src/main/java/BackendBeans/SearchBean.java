5a0450620b4df7b3726de3831940c13b789f903a
Andreia <patrocinoandreia@ua.pt>
2018-07-08 22:39:58 +0100

    as funções que acedem à base de dados estão no Bean newSessionBean

--- goCamping/src/main/java/BackendBeans/SearchBean.java
+++ goCamping/src/main/java/BackendBeans/SearchBean.java
@@ -7,23 +7,12 @@ package BackendBeans;
 
 import Persistencia.Campsite;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import javax.annotation.Resource;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 
 /**
  *
@@ -48,12 +37,8 @@ public class SearchBean implements Serializable{
    @ManagedProperty(value = "#{checkout}")
     private Date checkout;
     
-            
-    @Resource
-    UserTransaction utx;
-
-    @PersistenceContext(unitName = "PUnit")
-    private EntityManager em;
+    @EJB
+    NewSessionBean newSessionBean;
     
     
     public String getCampsiteLocation() {
@@ -87,59 +72,17 @@ public class SearchBean implements Serializable{
 
     public void setCheckout(Date checkout) {
         this.checkout = checkout;
-    }
-    
-    public List<Campsite> listarTodosCampsites() {
-        List<Campsite> campsites = new ArrayList<>();
-        List<Campsite> list = new ArrayList<>();
-        try {
-            utx.begin();
-            Query query = getEntityManager().createQuery("select c from Campsite c");
-
-            campsites = query.getResultList();
-            for (Iterator<Campsite> iterator = campsites.iterator(); iterator.hasNext();) {
-                Campsite campsite = (Campsite) iterator.next();
-                list.add(campsite);
-            }
-            utx.commit();
-        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException e) {
-            System.out.println("listar campsite didn't work");
-        }
-        return list;
-    }
-
-    public List<Campsite> listarCampsite(String location) {
-        List<Campsite> campsites = new ArrayList<>();
-        List<Campsite> list = new ArrayList<>();
-        try {
-            utx.begin();
-            Query query = getEntityManager().createQuery("select c from Campsite as c where c.location like :location");
-            query.setParameter("location", "%" + location + "%");
-            campsites = query.getResultList();
-            for (Iterator<Campsite> iterator = campsites.iterator(); iterator.hasNext();) {
-                Campsite campsite = (Campsite) iterator.next();
-                System.out.println(campsite.getId() + " \t " + campsite.getTitle() + "\t" + campsite.getLocation());
-                list.add(campsite);
-            }
-            System.out.println("just before commit");
-            utx.commit();
-        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException e) {
-            System.out.println("listar campsite didn't work");
-        }
-        return list;
-    }
+    }  
   
     public String showAll(){
-        listaCampsites = listarTodosCampsites();     
+        listaCampsites = newSessionBean.listarTodosCampsites();     
         return "results.xhtml";
     }
     
     public String search(){
-        listaCampsites = listarCampsite(campsiteLocation); 
+        listaCampsites = newSessionBean.listarCampsite(campsiteLocation); 
         return "results.xhtml";
     }
     
-    protected EntityManager getEntityManager() {
-        return em;
-    }
+   
 }
